f11dc837e69d89b14765559b0d1c0fb458839995
Kuanis2006 Karakuzov <kukakarakuzov@gmail.com>
2024-03-28 08:08:42 +0300

    1.3v

---
+++ project_site/Views/Home/logreg.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace project_site.Views.Home
+{
+    public class logregModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
